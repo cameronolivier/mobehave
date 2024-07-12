401c3dd4b75a54f4e27f3203f4095bf287e4ac09
Cameron Olivier <cameron.olivier@mohara.co>
2024-07-12 21:56:05 +0200

    chore: setting up shadcn, jest and import sorting. in prog setting up husky

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,19 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ["prettier-plugin-tailwindcss", '@ianvs/prettier-plugin-sort-imports'],
+  importOrder: [
+    '<BUILT_IN_MODULES>',
+    '',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/(.*)$',
+    '',
+    '^../(.*)$',
+    '',
+    '^[./]',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.0.0',
 };
 
 export default config;
